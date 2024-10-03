84edaf35a94cd7cd90482d690ff6024ad989edf2
Jimmy CREUSAT <jimmy.creusat@gmail.com>
2024-10-03 14:45:56 +0200

    Add Mentions légales Page HTML + CSS + BOOTSTRAP

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import Mentions from './pages/Mentions';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Contact />
+      <Mentions />
       <Footer />
     </div>
   );
