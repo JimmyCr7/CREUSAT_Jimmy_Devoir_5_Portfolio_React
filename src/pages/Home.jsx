8d64a02f4dde4ae20d91f75cca14e90b1f7ca78b
Jimmy CREUSAT <jimmy.creusat@gmail.com>
2024-10-04 17:14:46 +0200

    CSS Conflicts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import Skill from '../components/Skill';
 import imgAbout from '../img/john-doe-about.jpg';
 import '../css/home-style.css';
 
@@ -6,13 +5,13 @@ const Home = () => {
     return(
         <main>
             <section>
-                <h1 class="text-white">Bonjour, je suis Jimmy Creusat</h1>
-                <h2 class="text-white">Développeur web fullstack</h2>
-                <button type="button" class="btn btn-danger">En savoir plus</button>
+                <h1 className="text-white">Bonjour, je suis Jimmy Creusat</h1>
+                <h2 className="text-white">Développeur web fullstack</h2>
+                <button type="button" className="btn btn-danger">En savoir plus</button>
             </section>
-            <section class="col-lg-4 col-sm-12">
+            <section className="col-lg-4 col-sm-12">
                 <h3>A propos</h3>
-                <hr class="border border-3 border-primary"/>
+                <hr className="border border-3 border-primary"/>
                 <img src={imgAbout} alt="Image d'une personne en train de travailler"/>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque tempor nisl. Vivamus vitae porttitor massa. Nunc augue magna, dictum non urna et, ultricies placerat ipsum. Donec vulputate sapien ut lorem convallis scelerisque.
@@ -24,35 +23,28 @@ const Home = () => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque tempor nisl. Vivamus vitae porttitor massa. Nunc augue magna, dictum non urna et, ultricies placerat ipsum. Donec vulputate sapien ut lorem convallis scelerisque.
                 </p>
             </section>
-            <section class="col-lg-4 col-sm-12">
+            <section className="col-lg-4 col-sm-12">
                 <h3>Mes compétences</h3>
-                <hr class="border border-3 border-primary" />
-                {/*
-                <Skill skillName="HTML5" percentage="90%" bgColor="bg-danger"/>
-                <Skill skillName="CSS3" percentage="80%" bgColor="bg-info"/>
-                <Skill skillName="JAVASCRIPT" percentage="70%" bgColor="bg-warning"/>
-                <Skill skillName="PHP" percentage="60%" bgColor="bg-success"/>
-                <Skill skillName="REACT" percentage="50%" bgColor="bg-primary"/>
-                */}
+                <hr className="border border-3 border-primary" />
                 <em>HTML5 90%</em>
-                <div class="progress">
-                    <div class="progress-bar bg-danger" role="progressbar" style={{width: "90%"}} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                <div className="progress">
+                    <div className="progress-bar bg-danger" role="progressbar" style={{width: "90%"}} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <em>CSS3 80%</em>
-                <div class="progress">
-                    <div class="progress-bar bg-info" role="progressbar" style={{width: "80%"}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                <div className="progress">
+                    <div className="progress-bar bg-info" role="progressbar" style={{width: "80%"}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <em>JAVASCRIPT 70%</em>
-                <div class="progress">
-                    <div class="progress-bar bg-warning" role="progressbar" style={{width: "70%"}} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                <div className="progress">
+                    <div className="progress-bar bg-warning" role="progressbar" style={{width: "70%"}} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <em>PHP 60%</em>
-                <div class="progress">
-                    <div class="progress-bar bg-success" role="progressbar" style={{width: "60%"}} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                <div className="progress">
+                    <div className="progress-bar bg-success" role="progressbar" style={{width: "60%"}} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <em>REACT 50%</em>
-                <div class="progress">
-                    <div class="progress-bar bg-primary" role="progressbar" style={{width: "50%"}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                <div className="progress">
+                    <div className="progress-bar bg-primary" role="progressbar" style={{width: "50%"}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
             </section>
         </main>
