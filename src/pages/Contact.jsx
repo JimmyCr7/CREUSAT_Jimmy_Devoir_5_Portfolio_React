f791aabd6c4340e06735ab45455eab072720bb95
Jimmy CREUSAT <jimmy.creusat@gmail.com>
2024-10-04 16:17:22 +0200

    revision contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,45 +2,46 @@ import imgMap from '../img/svg/map-logo.svg';
 import imgPosition from '../img/svg/position-logo.svg';
 import imgMobile from '../img/svg/mobile-logo.svg';
 import imgMail from '../img/svg/mail-logo.svg';
+import '../css/contact-style.css';
 
 const Contact = () => {
     return(
-        <main class="my-4">
-            <h2 class="text-center w-100">Contact</h2>
+        <main class="my-4 flex-column">
+            <h1 class="text-center w-100">Contact</h1>
             <p class="text-center w-100">Pour me contacter envue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact</p>
-            <hr class="border border-primary w-25 text-center d-block"/>
-            <div class="row justify-content-center">
-                <section class="col-lg-4 col-sm-12">
-                    <h3>Formulaire de contact</h3>
-                    <hr class="border border-primary"/>
+            <hr class="border border-3 border-primary w-25 d-block mx-auto"/>
+            <div class="row justify-content-center mt-3 mx-3">
+                <section class="col-lg-4 col-sm-12 mb-4">
+                    <h2>Formulaire de contact</h2>
+                    <hr class="border border-2 border-primary"/>
                     <form action="#" method="POST">
-                        <input class="form-control mb-2" type="text" placeholder="Votre nom"/>
-                        <input class="form-control mb-2" type="email" placeholder="Votre adresse email"/>
-                        <input class="form-control mb-2" type="tel" placeholder="Votre numéro de téléphone"/>
-                        <input class="form-control mb-2" type="text" placeholder="Sujet"/>
-                        <textarea class="form-control mb-2" name="messContact" id="messContact" placeholder="Votre message" rows="10"></textarea>
-                        <input class="btn btn-primary" type="submit" name="sendMessContact" value="Envoyer"/>
+                        <input class="form-control mb-2" type="text" placeholder="Votre nom" required/>
+                        <input class="form-control mb-2" type="email" placeholder="Votre adresse email" required/>
+                        <input class="form-control mb-2" type="tel" placeholder="Votre numéro de téléphone" required/>
+                        <input class="form-control mb-2" type="text" placeholder="Sujet" required/>
+                        <textarea class="form-control mb-2" name="messContact" id="messContact" placeholder="Votre message" rows="10" required></textarea>
+                        <input class="btn btn-primary d-block mx-auto" type="submit" name="sendMessContact" value="Envoyer"/>
                     </form>
                 </section>
                 <section class="col-lg-4 col-sm-12">
-                    <h3>Mes coordonnées</h3>
-                    <hr class="border border-primary" />
+                    <h2>Mes coordonnées</h2>
+                    <hr class="border border-2 border-primary" />
                     <strong>Jimmy Creusat</strong>
                     <address>
-                        <div class="row align-items-center">
-                            <img class="mr-1" src={imgMap} alt="Icone représentant une carte" />
+                        <div class="d-flex align-items-center">
+                            <img class="me-1 d-block" src={imgMap} alt="Icone représentant une carte" />
                             <p class="m-0">5 Place Colombe</p>
                         </div>
-                        <div class="row align-items-center">
-                            <img class="mr-1" src={imgPosition} alt="Icone représentant une position" />
+                        <div class="d-flex align-items-center">
+                            <img class="me-1" src={imgPosition} alt="Icone représentant une position" />
                             <p class="m-0">54700 Pont-à-Mousson</p>
                         </div>
-                        <div class="row align-items-center">
-                            <img class="mr-1" src={imgMobile} alt="Icone représentant un mobile" />
+                        <div class="d-flex align-items-center">
+                            <img class="me-1" src={imgMobile} alt="Icone représentant un mobile" />
                             <p class="m-0">0645606084</p>
                         </div>
-                        <div class="row align-items-center">
-                            <img class="mr-1" src={imgMail} alt="Icone représentant une enveloppe" />
+                        <div class="d-flex align-items-center">
+                            <img class="me-1" src={imgMail} alt="Icone représentant une enveloppe" />
                             <p class="m-0">jimmy.creusat@gmail.com</p>
                         </div>
                     </address>
